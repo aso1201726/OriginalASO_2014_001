2f34278bb65e5af214761e8373bb6b57a4238a09
student <student@PC111S19.fgakusei.local>
2014-06-02 13:42:51 +0900

    編集済み

--- gen/com/example/originalaso_2014_001/R.java
+++ gen/com/example/originalaso_2014_001/R.java
@@ -22,19 +22,35 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int gazou01=0x7f020000;
+        public static final int gazou02=0x7f020001;
+        public static final int gazou03=0x7f020002;
+        public static final int gazou04=0x7f020003;
+        public static final int gazou05=0x7f020004;
+        public static final int gazou06=0x7f020005;
+        public static final int gazou07=0x7f020006;
+        public static final int gazou08=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int title=0x7f020009;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f080001;
         public static final int Name=0x7f080003;
         public static final int Txt1=0x7f080002;
-        public static final int action_settings=0x7f080005;
-        public static final int btnGO=0x7f080004;
+        public static final int action_settings=0x7f080008;
+        public static final int btnGO=0x7f080005;
         public static final int container=0x7f080000;
+        public static final int imageView1=0x7f080004;
+        public static final int textView1=0x7f080006;
+        public static final int textView2=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_main=0x7f030001;
+        public static final int mikuji_01=0x7f030002;
+        public static final int mikuji_02=0x7f030003;
+        public static final int mikuji_03=0x7f030004;
+        public static final int mikuji_04=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/example/originalaso_2014_001/mikuji04.java
+++ src/com/example/originalaso_2014_001/mikuji04.java
@@ -1,7 +1,15 @@
 package com.example.originalaso_2014_001;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class mikuji04 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mikuji_04);
+	}
 
 }
